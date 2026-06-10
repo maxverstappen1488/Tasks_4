58d7c4e5ab6b86175c340f906f9d36b9d3b86c63
Максим Борисов ТКИ-242 <maks.borisov858@gmail.com>
2026-06-10 21:00:55 +0300

    Update 2.cpp

--- 2.cpp
+++ 2.cpp
@@ -1,8 +1,6 @@
 #include <iostream>
-#include <list>
-#include <numeric>   // для std::iota
-#include <cstdlib>   // для rand, srand
-#include <ctime>     // для time
+#include <list> 
+#include <iterator> 
 
 using namespace std;
 
@@ -10,12 +8,7 @@ using namespace std;
  * @brief выводит элементы списка в консоль
  * @param L - список целых чисел для вывода
  */
-void print_list(const list<int>& L) {
-    for (const int& i : L) {
-        cout << i << ' ';
-    }
-    cout << endl;
-}
+void print_list(const list<int>& L);
 
 /**
  * @brief удаляет элементы с нечетными индексами из первой половины списка
@@ -23,14 +16,7 @@ void print_list(const list<int>& L) {
  * @details Функция находит середину списка и удаляет каждый второй элемент
  *          (с нечетными индексами: 1, 3, 5...) из первой половины списка
  */
-void del_first_half_odd_indexes(list<int>& L) {
-    auto end = L.begin();
-    advance(end, L.size() / 2);
-    for (auto it = L.begin(); it != end; ++it) {
-        it = L.erase(it);   // удаляем текущий элемент, it указывает на следующий
-        if (it == end) break;
-    }
-}
+void del_first_half_odd_indexes(list<int>& L); 
 
 /**
  * @brief точка входа в программу
@@ -39,16 +25,33 @@ void del_first_half_odd_indexes(list<int>& L) {
  *          заполняет его последовательными числами, выводит исходный список,
  *          применяет алгоритм удаления и выводит результат
  */
+
 int main() {
     srand(time(0));
-    size_t size = (rand() % 9 + 1) * 4;   // от 4 до 36
+    size_t size = (rand() % 9 + 1) * 4;   // от 4 до 36 
 
-    list<int> L(size);                   // создаём список нужного размера
-    iota(L.begin(), L.end(), 1);         // заполняем числами 1,2,...,size
+    list<int> L;                   
+    copy_n(istream_iterator<int>(cin), size, back_inserter(L));    
 
-    print_list(L);                       // вывод исходных данных
-    del_first_half_odd_indexes(L);       // алгоритм по заданию
-    print_list(L);                       // вывод результата
+    print_list(L);                       // вывод исходных данных 
+    del_first_half_odd_indexes(L);       // алгоритм по заданию 
+    print_list(L);                       // вывод результата 
 
     return 0;
+}
+
+void print_list(const list<int>& L) {
+    for (const int& i : L) {
+        cout << i << ' ';
+    }
+    cout << endl;
+}
+
+void del_first_half_odd_indexes(list<int>& L) {
+    auto end = L.begin();
+    advance(end, L.size() / 2);
+    for (auto it = L.begin(); it != end; ++it) {
+        it = L.erase(it);   // удаляем текущий элемент, it указывает на следующий 
+        if (it == end) break;
+    }
 }
